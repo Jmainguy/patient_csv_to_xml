83df6130e5c175cdbb015e6508389427df39c234
Jonathan Mainguy <jon@soh.re>
2018-04-09 11:53:20 -0400

    formatting telephone numbers now

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ func formatBirthDate(month, day, year string) (birthday string) {
     return
 }
 
+func formatPhoneNumber(areacode, number string) (phonenumber string) {
+    lastFour := number[3:len(number)]
+    firstThree := number[0:3]
+    phonenumber = fmt.Sprintf("%s-%s-%s", areacode, firstThree, lastFour)
+    return
+}
+
 func main() {
 
     filename := "/tmp/jon.csv"
@@ -43,9 +50,12 @@ func main() {
         fullPatBirthDate := ""
         fullSrvFromDate := ""
         fullSrvToDate := ""
+        fullInsBirthDate := ""
         insFirstName := ""
         insLastName := ""
         formatedSrvCharges := ""
+        fullPatPhoneNo := ""
+        fullInsPhone := ""
         if line[10] != "" {
             fullPatBirthDate = formatBirthDate(line[10],line[11],line[12])
         }
@@ -54,6 +64,9 @@ func main() {
         }
         if line[109] != "" {
             fullSrvToDate = formatBirthDate(line[109],line[110],line[111])
+        }
+        if line[37] != "" {
+            fullInsBirthDate = formatBirthDate(line[37],line[38],line[39])
         }
         if line[15] != "" {
             insFullName := strings.Split(rm_lead_space(line[15]), " ")
@@ -65,24 +78,30 @@ func main() {
         if line[118] != "" {
             formatedSrvCharges = fmt.Sprintf("%s.%s", rm_lead_space(line[118]), rm_lead_space(line[119]))
         }
+        if line[27] != "" {
+            fullPatPhoneNo = formatPhoneNumber(rm_lead_space(line[27]), rm_lead_space(line[28]))
+        }
+        if line[30] != "" {
+            fullInsPhone = formatPhoneNumber(rm_lead_space(line[30]), rm_lead_space(line[31]))
+        }
         data := Patient{
             PatFirstName: rm_lead_space(patFullName[1]),
             PatLastName: patFullName[0],
             PatAddress: rm_lead_space(line[16]),
             PatBirthDate: fullPatBirthDate,
             PatCity: rm_lead_space(line[22]),
-            PatPhoneNo: rm_lead_space(line[27]),
+            PatPhoneNo: fullPatPhoneNo,
             PatSigOnFile: "1",
             PatState: rm_lead_space(line[23]),
             PatZip: rm_lead_space(line[26]),
             Patient_Insured: Patient_Insured{
                 InsAddress: rm_lead_space(line[21]),
-                InsBirthDate: rm_lead_space(line[37]),
+                InsBirthDate: fullInsBirthDate,
                 InsCity: rm_lead_space(line[24]),
                 InsFirstName: insFirstName,
                 InsLastName: insLastName,
                 InsIDNumber: rm_lead_space(line[8]),
-                InsPhone: rm_lead_space(line[30]),
+                InsPhone: fullInsPhone,
                 InsState: rm_lead_space(line[25]),
                 InsZip: rm_lead_space(line[29]),
                 PatInsRelationToInsured: "1",
