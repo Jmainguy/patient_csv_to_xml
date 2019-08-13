5cd50952e4308c17c5284612d195f964af561861
Jonathan Mainguy <jon@soh.re>
2019-08-13 21:41:14 +0200

    use the hub

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker kill patient_csv_to_xml
 docker rm patient_csv_to_xml
-docker run --name patient_csv_to_xml --restart always -p 8000:8000 -d patient_csv_to_xml 
+docker run --name patient_csv_to_xml --restart always -p 8000:8000 -d hub.soh.re/soh.re/patient_csv_to_xml 
